57a98aec23d96dd579da06451f2356237dbf2035
freek <freek@spatie.be>
2015-07-18 00:53:23 +0200

    fix class name

--- src/CacheProfiles/CacheAllSuccessFulSuccessfulGetRequests.php
+++ src/CacheProfiles/CacheAllSuccessFulSuccessfulGetRequests.php
@@ -5,7 +5,7 @@ namespace Spatie\ResponseCache\CacheProfiles;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CacheAllSuccessFulSuccessfulGetRequests extends BaseCacheProfile implements CacheProfile
+class CacheAllSuccessFulGetRequests extends BaseCacheProfile implements CacheProfile
 {
     /**
      * Determine if the given request should be cached;.
