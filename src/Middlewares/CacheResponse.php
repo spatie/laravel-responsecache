a837fd626a0532a3f438c04951a71fcf61045a13
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-03-16 14:23:31 +0100

    StyleCI

--- src/Middlewares/CacheResponse.php
+++ src/Middlewares/CacheResponse.php
@@ -5,8 +5,8 @@ namespace Spatie\ResponseCache\Middlewares;
 use Closure;
 use Illuminate\Http\Request;
 use Spatie\ResponseCache\ResponseCache;
-use Symfony\Component\HttpFoundation\Response;
 use Spatie\ResponseCache\Events\CacheMissed;
+use Symfony\Component\HttpFoundation\Response;
 use Spatie\ResponseCache\Events\ResponseCacheHit;
 
 class CacheResponse
