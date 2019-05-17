7a711435dc08786f02be807602adaf4b02e4c390
Freek Van der Herten <freek@spatie.be>
2019-05-17 16:56:30 +0000

    Apply fixes from StyleCI

--- src/CacheProfiles/CacheAllSuccessfulGetRequests.php
+++ src/CacheProfiles/CacheAllSuccessfulGetRequests.php
@@ -2,8 +2,8 @@
 
 namespace Spatie\ResponseCache\CacheProfiles;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CacheAllSuccessfulGetRequests extends BaseCacheProfile
