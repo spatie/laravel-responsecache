f979d2425cc8de07eb8ab914124b2e5e483a8ecf
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-06 23:09:14 +0300

    refactor ResponseCacheRepositoryTest

--- tests/ResponseCacheRepositoryTest.php
+++ tests/ResponseCacheRepositoryTest.php
@@ -8,20 +8,15 @@ use Spatie\ResponseCache\Exceptions\CouldNotUnserialize;
 use Spatie\ResponseCache\ResponseCacheRepository;
 use Spatie\ResponseCache\Serializers\Serializer;
 
-class ResponseCacheRepositoryTest extends TestCase
-{
-    /** @test */
-    public function it_handles_missed_cache_gracefully()
-    {
-        // Instantiate a default serializer
-        $responseSerializer = app(Serializer::class);
+test('it handles missed cache gracefully', function () {
+    // Instantiate a default serializer
+    $responseSerializer = app(Serializer::class);
 
-        $cacheRepository = Mockery::mock(Repository::class);
-        $cacheRepository->shouldReceive('get')->with('missed-cache')->once()->andReturn(null);
+    $cacheRepository = Mockery::mock(Repository::class);
+    $cacheRepository->shouldReceive('get')->with('missed-cache')->once()->andReturn(null);
 
-        $this->expectException(CouldNotUnserialize::class);
+    $this->expectException(CouldNotUnserialize::class);
 
-        $repository = new ResponseCacheRepository($responseSerializer, $cacheRepository);
-        $repository->get('missed-cache');
-    }
-}
+    $repository = new ResponseCacheRepository($responseSerializer, $cacheRepository);
+    $repository->get('missed-cache');
+});
