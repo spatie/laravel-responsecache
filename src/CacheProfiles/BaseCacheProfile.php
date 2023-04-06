fcab4f62eee7feb3d7b2f70d4f0a5d7241d7a1ab
Dóra Kékesi <dorqa95@users.noreply.github.com>
2023-04-06 17:29:41 +0200

    Fix missing Route import

--- src/CacheProfiles/BaseCacheProfile.php
+++ src/CacheProfiles/BaseCacheProfile.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 abstract class BaseCacheProfile implements CacheProfile
 {
