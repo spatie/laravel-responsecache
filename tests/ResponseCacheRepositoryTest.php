8f53ee8729376bbd33ee5671fcad8c0d16dd639d
Steven Wichers <swichers@users.noreply.github.com>
2023-04-07 01:26:23 -0700

    Issue #342: CacheResponse race condition with has and get (#434)
    * Issue #342: Resolve issue with has/get race condition
    
    * Issue #342: Align more closely with original code
    
    * Update CacheResponse.php
    
    ---------
    
    Co-authored-by: Freek Van der Herten <freek@spatie.be>

--- tests/ResponseCacheRepositoryTest.php
+++ tests/ResponseCacheRepositoryTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Cache\Repository;
+use Illuminate\Testing\TestResponse;
 use Spatie\ResponseCache\Exceptions\CouldNotUnserialize;
 use Spatie\ResponseCache\ResponseCacheRepository;
 use Spatie\ResponseCache\Serializers\Serializer;
@@ -15,3 +16,36 @@ it('handles missed cache gracefully', function () {
     $repository = new ResponseCacheRepository($responseSerializer, $cacheRepository);
     $repository->get('missed-cache');
 })->throws(CouldNotUnserialize::class);
+
+it('will handle race conditions between has and get', function () {
+
+    /** @var Serializer $responseSerializer */
+    $responseSerializer = app(Serializer::class);
+    /** @var Illuminate\Cache\ArrayStore $cacheStore */
+    $cacheStore = app('cache')
+        ->store('array')
+        ->getStore();
+
+    // This order of operations simulates a cache lookup happening during a
+    // cache expiration or purge event. The `has()` call should succeed, but
+    // after that the cache has 'expired' and is unavailable.
+    $cachedValues = [
+        $responseSerializer->serialize(createResponse(200)),
+        null,
+    ];
+
+    // We cannot use the partialMock helper because the cache store must be
+    // available and partialMock does not allow constructor arguments.
+    $cacheRepository = Mockery::mock(Repository::class, [$cacheStore]);
+    $cacheRepository
+            ->shouldReceive('get')
+            ->twice()
+            ->andReturns($cachedValues);
+    $cacheRepository->makePartial();
+    $this->instance(Repository::class, $cacheRepository);
+
+    /** @var TestResponse $response */
+    $response = $this->get('/random');
+    assertRegularResponse($response);
+    expect($response->exception)->toBeNull();
+});
